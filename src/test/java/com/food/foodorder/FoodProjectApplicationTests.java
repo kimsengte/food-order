564deb5fe8d08dca6e825050bcfce0d99c9314b3
kimsengte <kimsengte@gmail.com>
2021-12-13 19:27:52 +0700

    first commit

---
+++ src/test/java/com/food/foodorder/FoodProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food.foodorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
